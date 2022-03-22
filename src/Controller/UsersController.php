604156c6eb29f149a4de664638ac13f2690f3681
Rob Nugen <hi.robnugen.com@robnugen.com>
2022-03-21 19:29:37 -0700

    Prevent infinite loop when logging in

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,6 +11,45 @@ namespace App\Controller;
  */
 class UsersController extends AppController
 {
+
+    /**
+     * Prevent infinite loop when not logged in
+     *
+     */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        // Configure the login action to not require authentication, preventing
+        // the infinite redirect loop issue
+        $this->Authentication->addUnauthenticatedActions(['login']);
+    }
+
+    /**
+     * Allow us to log in
+     *
+     * @return \Cake\Http\Response|null|void Renders view   (I GUESS)
+     */
+    public function login()
+    {
+        $this->request->allowMethod(['get', 'post']);
+        $result = $this->Authentication->getResult();
+        // regardless of POST or GET, redirect if user is logged in
+        if ($result->isValid()) {
+            // redirect to /articles after login success
+            $redirect = $this->request->getQuery('redirect', [
+                'controller' => 'Articles',
+                'action' => 'index',
+            ]);
+
+            return $this->redirect($redirect);
+        }
+        // display error if user submitted and authentication failed
+        if ($this->request->is('post') && !$result->isValid()) {
+            $this->Flash->error(__('Invalid username or password'));
+        }
+    }
+
+
     /**
      * Index method
      *
